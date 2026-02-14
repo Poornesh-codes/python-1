837a50aa916f61829badb680d12680148a0ada49
Poornesh-codes <poorneshk9@gmail.com>
2026-02-14 18:40:00 +0530

    first commit

---
+++ assigment1.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+#add
+print("addition :",a+b)
+#subtract
+print("subtraction :",a-b)
+#multiply
+print("multiplication :",a*b)
+#divide
+print("division :",a/b)
---
+++ assignment_task_2.py
@@ -0,0 +1,4 @@
+a=input("Enter your first name:")
+b=input("Enter your last name:")
+name=a+" "+b
+print("hello,"+ name +" welcome to py program.")
